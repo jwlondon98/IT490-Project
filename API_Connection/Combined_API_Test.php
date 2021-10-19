6a1545869b75d00301b1d3d4ea130a07e26ea17f
jwlondon98 <jwlondon98@gmail.com>
2021-10-19 13:05:09 -0400

    combined api test working

---
+++ API_Connection/Combined_API_Test.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'Quote_API_Test.php';
+$quoteJSON = GetQuote();
+$quote = $quoteJSON['content'];
+$origin = $quoteJSON['originator'];
+$quoteURL = $quoteJSON['url'];
+echo "\n" . $quote . "\t\n" . $origin['name'] . "\t\n". $quoteURL ."\n\n";
+
+include 'TextAnalysis_API_Test.php';
+PerformTextAnalysis($quoteURL);
--- API_Connection/TextAnalysis_API_Test.php
+++ API_Connection/TextAnalysis_API_Test.php
@@ -1,20 +1,22 @@
 <?php
 
-function GetQuote()
+function PerformTextAnalysis ($quote)
 {
 	$curl = curl_init();
-
+	
 	curl_setopt_array($curl, [
-		CURLOPT_URL => "https://quotes15.p.rapidapi.com/quotes/random/",
+		CURLOPT_URL => "https://text-analysis12.p.rapidapi.com/ner/api/v1.1",
 		CURLOPT_RETURNTRANSFER => true,
 		CURLOPT_FOLLOWLOCATION => true,
 		CURLOPT_ENCODING => "",
 		CURLOPT_MAXREDIRS => 10,
 		CURLOPT_TIMEOUT => 30,
 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_CUSTOMREQUEST => "POST",
+		CURLOPT_POSTFIELDS => "{\n    \"language\": \"english\",\n    \"text\": \" . $quote .\\n\"\n}",
 		CURLOPT_HTTPHEADER => [
-			"x-rapidapi-host: quotes15.p.rapidapi.com",
+			"content-type: application/json",
+			"x-rapidapi-host: text-analysis12.p.rapidapi.com",
 			"x-rapidapi-key: 858a670fb5msh2f906f02d058ee4p133dccjsn356293c3b633"
 		],
 	]);
@@ -24,13 +26,9 @@ function GetQuote()
 	
 	curl_close($curl);
 	
-	if ($err) 
-	{
-		return "cURL Error #:" . $err;
-	} 
-	else 
-	{
-		$json = json_decode($response, true);
-		return $json;
+	if ($err) {
+		echo "cURL Error #:" . $err;
+	} else {
+		echo $response;
 	}
 }
