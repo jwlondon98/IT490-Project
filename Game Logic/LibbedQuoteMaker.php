5df72b613317f3572ee0922f9703478d47941d66
jwlondon98 <jwlondon98@gmail.com>
2021-11-07 23:42:31 -0500

    modified apiRabbitMQServer to get quote

--- Game Logic/LibbedQuoteMaker.php
+++ Game Logic/LibbedQuoteMaker.php
@@ -2,21 +2,23 @@
 
 include 'QuoteGetter.php';
 
-MakeLibbedQuote();
+// MakeLibbedQuote();
 
 function MakeLibbedQuote()
 {
     $quoteObj = GetLibbedQuote();
     $posArr = $quoteObj->posArray;
     $posArrCount = count($posArr);
-    echo $quoteObj->quote;
+    // echo $quoteObj->quote;
     $retArr = ProcessPOSArray($quoteObj->posArray);
 
-    for ($i = 0; $i < $posArrCount; $i++)
-    {
-        echo $retArr[$i] . " ";
-    }
-    echo "\n\n";
+    // for ($i = 0; $i < count($retArr); $i++)
+    // {
+    //     echo $retArr[$i] . " ";
+    // }
+    // echo "\n\n";
+
+    return $retArr;
 }
 
 function ProcessPOSArray($arr)
@@ -24,9 +26,9 @@ function ProcessPOSArray($arr)
     // echo "\nPROCESSING POS ARRAY\n";
     
     // nouns, verbs, adjectives
-    $tagsToCheck = array("NN", "NNS", "NNP", "NNPS", 
-        "VB");//, "VBD", "VBG", "VBN", "VBP", "VBZ");
-        // "JJ", "JJR", "JJS");
+    $nounTagsToCheck = array("NN", "NNS", "NNP", "NNPS");
+    $verbTagsToCheck = array("VB");//, "VBD", "VBG", "VBN", "VBP", "VBZ");
+    // $adjectiveTagsToCheck = array("JJ");// "JJ", "JJR", "JJS");
 
     $numPOSElems = 0;
     $arrCount = count($arr);
@@ -38,10 +40,7 @@ function ProcessPOSArray($arr)
         $maxPOSElements = 10;
 
     $returnArr = array();
-    $normalArr = array();
-    $matchedPOSArr = array();
 
-    $prevMatchedIndex = -1;
     // for each element in the POS array
     for ($i = 0; $i < $arrCount; $i++)
     {
@@ -50,29 +49,37 @@ function ProcessPOSArray($arr)
         $tag = $arr[$i]['tag'];
 
         // if tag = current tag to compare 
-        if (in_array($tag, $tagsToCheck) &&
-            $numPOSElems < $maxPOSElements &&
-            $prevMatchedIndex != $i - 1)
+        if (in_array($tag, $nounTagsToCheck) &&
+            $numPOSElems < $maxPOSElements)
         {
-            $matchedPOSArr[$i] = $text; 
-            $prevMatchedIndex = $i;
             // echo ("|| " . $text . "\n");
+            $returnArr[$i] = "NOUN";
+
+            $numPOSElems++;
+            // echo ("\ntext: " . $text . '  tag: ' . $tag);
+        }
+        else if (in_array($tag, $verbTagsToCheck) &&
+            $numPOSElems < $maxPOSElements)
+        {
+            // echo ("|| " . $text . "\n");
+            $returnArr[$i] = "VERB";
+
             $numPOSElems++;
             // echo ("\ntext: " . $text . '  tag: ' . $tag);
         }
         else 
         {
-            $normalArr[$i] = $text;
+            $returnArr[$i] = $text;
         }
     }
 
-    for ($j = 0; $j < $arrCount; $j++)
-    {
-        if (array_key_exists($j, $normalArr))
-            $returnArr[$j] = $normalArr[$j];
-        else if (array_key_exists($j, $matchedPOSArr))
-            $returnArr[$j] = "___";
-    }    
+    // for ($j = 0; $j < $arrCount; $j++)
+    // {
+    //     if (array_key_exists($j, $normalArr))
+    //         $returnArr[$j] = $normalArr[$j];
+    //     else if (array_key_exists($j, $matchedPOSArr))
+    //         $returnArr[$j] = "___";
+    // }    
 
     echo "\n";
 
