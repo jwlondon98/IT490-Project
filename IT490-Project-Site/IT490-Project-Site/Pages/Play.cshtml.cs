6d03bc0d08ace64ae7ca009e55a8a35734330744
Jordan London <jwlondon98@gmail.com>
2021-11-08 12:31:41 -0500

    simple play page created

---
+++ IT490-Project-Site/IT490-Project-Site/Pages/Play.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IT490_Project_Site.Pages
+{
+    public class PlayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
