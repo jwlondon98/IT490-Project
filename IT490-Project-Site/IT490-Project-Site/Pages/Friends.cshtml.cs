de59fd3553dfd5e3a2f136c0ca12c3bde39f2608
Jordan London <jwlondon98@gmail.com>
2021-11-09 15:59:48 -0500

    friends page now has get/post request framework

--- IT490-Project-Site/IT490-Project-Site/Pages/Friends.cshtml.cs
+++ IT490-Project-Site/IT490-Project-Site/Pages/Friends.cshtml.cs
@@ -1,16 +1,44 @@
 using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace IT490_Project_Site.Pages
 {
     public class FriendsModel : PageModel
     {
+        public string[] friendRequestNames = { 
+          "john", "james", "jack"  
+        };
+
+        public string test = "this is a test";
+
         public void OnGet()
         {
+            Console.WriteLine("GET");
+        }
+
+        public void OnPost()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAddFriend(string friendUsername)
+        {
+            Console.WriteLine("friend added: " + friendUsername);
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostAcceptFriend(string friendUsername)
+        {
+            Console.WriteLine("friend accepted: " + friendUsername);
+
+            return RedirectToPage();
         }
     }
 }
