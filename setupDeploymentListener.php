b7b078504bf3336cdb72b87b2c16bf91757d97b8
EricKnudsen256 <ek256@njit.edu>
2021-12-14 21:47:55 -0500

    Small changes to setup 10

--- setupDeploymentListener.php
+++ setupDeploymentListener.php
@@ -27,6 +27,7 @@ shell_exec("sudo apt-get install -y php-ssh2");
 // # link deploy script to /usr/local/bin
 
 shell_exec("sudo ln -s deploy /usr/local/bin/deploy");
+copy("./deploy", "usr/local/bin/deploy");
 
 // # ln -l deploy /usr/local/bin/deploy
 
